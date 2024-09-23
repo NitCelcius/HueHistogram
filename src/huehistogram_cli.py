a79c208d78e98cce4c0191903c1a12ae86621236
NitCelcius <nitcelcius@nitcelcius.me>
2024-09-23 15:31:53 +0900

    Feature(DESTR.): path based output
    (破壊的変更あるよ)
    -o オプションはディレクトリとファイル名のいずれも受け付けるようにした
    破壊的変更: GenerateHistogram の引数 output_dir (出力先ディレクトリ)が output_path (出力先のパス) に変更

--- src/huehistogram_cli.py
+++ src/huehistogram_cli.py
@@ -3,13 +3,49 @@ import asyncio
 import os
 import sys
 import argparse
+from typing import Tuple
 
 from Huehistogram import Huehistogram
 
 SIG_INVALID_ARGS = 1
 
-if __name__ == "__main__":
 
+def input_and_output_extract_args(
+    input_arg: str | list[str], output_arg: str
+) -> Tuple[list[str], list[str]]:
+    if isinstance(input_arg, str):
+        # input_arg: list[str] を保証
+        return input_and_output_extract_args([input_arg], output_arg)
+    
+    if os.path.isdir(output_arg): # 存在するディレクトリならここに入れれば OK
+        output_paths = [
+            os.path.join(output_arg, os.path.basename(input_file))
+            for input_file in input_arg
+        ]
+        return input_arg, output_paths
+    
+    # output_arg は存在するディレクトリではないので、ファイルか存在しないディレクトリ
+    # つまりここに書き込むことはできない
+    output_file_name = os.path.basename(output_arg)
+    if output_file_name == "":  # output_arg がファイルじゃないなら
+        if len(input_arg) >= 2: # のわりに入力ファイルが2つ以上あるとどの名前を使えばいいか不明
+            # これは input_arg が str の場合も含むけど list で包括的に処理したかった
+            print(
+                f"If you are trying to input two or more files, "
+                f"use --output-dir option or specify a path to "
+                f"existing directory on --output option."
+            )
+            exit(SIG_INVALID_ARGS)
+        # 入力ファイルが1つなので、出力ファイルも同じ名前を使わせる
+        output_file_name = os.path.basename(input_arg[0])
+    
+    # output 先のディレクトリに output_file_name なるファイルを作れということと解釈
+    output_paths = [os.path.join(os.path.dirname(output_arg), output_file_name)]
+    return input_arg, output_paths
+
+
+if __name__ == "__main__":
+    # TODO: これ外に出す
     def show_help():
         print("Usage:")
         print("  To show color histogram:")
@@ -37,7 +73,9 @@ if __name__ == "__main__":
         nargs="+",
     )
     parser.add_argument(
-        "-o", "--output", help="path(s) to the directory to output histograms"
+        "-o",
+        "--output",
+        help="path(s) to the directory to output histogram(s). Specify a path to image file to input just one image, otherwise specify a path to existing directory.",
     )
     parser.add_argument(
         "-v", "--verbose", help="display verbose messages", action="store_true"
@@ -52,61 +90,64 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     input_files = []
-    output_dir = ""
     output_dpi = args.dpi
-
     is_verbose = args.verbose
-    if args.input is not None and args.output is not None:
-        if args.args:
-            print(
-                "Extra arguments are supplied! If you intend to use --input and --output options, please remove extra arguments other than -i and -o options."
-            )
-            print("Use:")
-            print(
-                "  python huehistogram_cli.py -i [path/to/image.jpg] [image_2.jpg] -o [path/to/histograms/]"
-            )
-            exit(SIG_INVALID_ARGS)
-        else:
-            input_files = args.input
-            output_dir = args.output
-    else:
-        if args.directory_input:
-            input_path = args.directory_input[0]
-            input_files = [
-                os.path.join(input_path, file_name)
-                for file_name in os.listdir(input_path)
-            ]
-            output_dir = args.output
-        elif args.input is None and args.output is None:
-            input_files = sys.argv[
-                1:-1
-            ]  # 実行するプログラム名と最後を除いて input だと思い込む
-            output_dir = sys.argv[-1]  # 最後は output だと思い込む
-        else:
-            print("Please specify paths to input files and output directory!")
-            print("Use:")
-            print(
-                "  python huehistogram_cli.py -i [path/to/image.jpg] [image_2.jpg] -o [path/to/histograms/]"
-            )
-            exit(SIG_INVALID_ARGS)
+    input_arg = args.input
+    output_arg = args.output
 
-    if not (input_files is not None and output_dir is not None):
+    if args.directory_input:
+        # directory_input, input は mutually exclusive なので input_arg = None であることが保証される
+        input_path = args.directory_input[0]
+        input_arg = [ # 子要素ぜんぶ input に指定したことにする
+            os.path.join(input_path, file_name) for file_name in os.listdir(input_path)
+        ]
+
+    if input_arg is not None and output_arg is not None and args.args:
+        print(
+            "Extra arguments are supplied! "
+            "If you intend to use --input and --output options, "
+            "please remove extra arguments other than -i and -o options."
+        )
+        print("Use:")
+        print(
+            "  python huehistogram_cli.py -i [path/to/image.jpg] [image_2.jpg] -o [path/to/histograms/]"
+        )
+        exit(SIG_INVALID_ARGS)
+
+    if input_arg is None and output_arg is None:
+        # Without explicit input or output files
+        input_arg = sys.argv[
+            1:-1
+        ]  # 実行するプログラム名と最後を除いて input だと思い込む
+        output_arg = sys.argv[-1]  # 最後は output だと思い込む
+
+    if output_arg is None:
+        print("Please specify a path to output histograms using --output option!")
+        exit(SIG_INVALID_ARGS)
+    
+    input_files, output_paths = input_and_output_extract_args(input_arg, output_arg)
+
+    if not input_files or not output_paths:
         print("Please specify paths to input files and output directory!")
         exit(SIG_INVALID_ARGS)
 
-    if not os.path.exists(output_dir):
-        print(f"Output directory {output_dir} does not exist!")
-        exit(SIG_INVALID_ARGS)
+    for output_file in output_paths:
+        if not os.path.isdir(os.path.dirname(output_file)):
+            print(f"Output directory {output_file} does not exist!")
+            exit(SIG_INVALID_ARGS)
 
     for input_file in input_files:
         if not os.path.exists(input_file):
             print(f"Input file {input_file} does not exist!")
             exit(SIG_INVALID_ARGS)
 
+    print(input_files)
+    print(output_paths)
+
     res = asyncio.run(
         Huehistogram.generate_hue_histograms(
             input_files=input_files,
-            output_dir=output_dir,
+            output_paths=output_paths,
             out_dpi=output_dpi,
             verbose=is_verbose,
             allow_overwrite=args.force_overwrite,
